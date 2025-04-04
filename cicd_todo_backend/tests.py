ba57530980c93f9968d030fabe398e70fd69da1d
ralph <oloresralph@gmail.com>
2025-04-04 19:11:43 +1300

    Modified test cases file

--- cicd_todo_backend/tests.py
+++ cicd_todo_backend/tests.py
@@ -1,7 +1,30 @@
+from django.db import connections
 from rest_framework.test import APITestCase
-from django.shortcuts import render
+from rest_framework import status
 from .models import Todo
-class TodoTests(APITestCase):
-    def todo_list(request):
-        todos = Todo.objects.all()
-        return render(request, 'todo_list.html', {'todos': todos})
+
+class TodoApiTests(APITestCase):
+
+    @classmethod
+    def tearDownClass(self):
+        connections.close_all()
+    def setUp(self):
+        """
+        Set up method called before each test.
+        Here we create a sample Todo object.
+        """
+        self.todo = Todo.objects.create(
+            title="Test Todo",
+            content="This is a test todo item."
+        )
+    def test_todo_creation(self):
+        """
+        Test that the Todo object is created correctly in the database.
+        """
+        # Retrieve the Todo object we created
+        todo = Todo.objects.get(title="Test Todo")
+
+        # Check that the title and content are correct
+        self.assertEqual(todo.title, "Test Todo")
+        self.assertEqual(todo.content, "This is a test todo item.")
+        self.assertFalse(todo.completed)  # Default value should be False
