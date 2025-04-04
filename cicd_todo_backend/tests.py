b4f19ccad90e5613db643fc14e9285bfdf7b70a7
ralph <oloresralph@gmail.com>
2025-04-04 19:14:55 +1300

    Modified test cases

--- cicd_todo_backend/tests.py
+++ cicd_todo_backend/tests.py
@@ -2,6 +2,7 @@ from django.db import connections
 from rest_framework.test import APITestCase
 from rest_framework import status
 from .models import Todo
+from django.utils import timezone
 
 class TodoApiTests(APITestCase):
 
@@ -28,3 +29,37 @@ class TodoApiTests(APITestCase):
         self.assertEqual(todo.title, "Test Todo")
         self.assertEqual(todo.content, "This is a test todo item.")
         self.assertFalse(todo.completed)  # Default value should be False
+
+    def test_todo_default_values(self):
+        """
+        Test that the default values (completed, created_at, updated_at) are set correctly.
+        """
+        todo = self.todo  # This is the Todo we created in setUp
+
+        # Check default completed value
+        self.assertFalse(todo.completed)
+
+        # Check that created_at is set and is a datetime
+        self.assertIsInstance(todo.created_at, timezone.datetime)
+        self.assertIsInstance(todo.updated_at, timezone.datetime)
+
+        # Ensure created_at and updated_at are not the same (since updated_at should change)
+        self.assertNotEqual(todo.created_at, todo.updated_at)
+
+    def test_update_todo(self):
+        """
+        Test updating a Todo item.
+        """
+        todo = self.todo
+        # Update the Todo object
+        todo.completed = True
+        todo.save()
+
+        # Reload the object from the database
+        updated_todo = Todo.objects.get(id=todo.id)
+
+        # Check that the 'completed' field has been updated
+        self.assertTrue(updated_todo.completed)
+
+        # Ensure updated_at is now different (because it's automatically updated)
+        self.assertNotEqual(todo.updated_at, updated_todo.updated_at)
